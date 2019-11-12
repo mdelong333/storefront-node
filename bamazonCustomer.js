c0a50d9fff2a5d7d8c6f0c360551cfb2ace7f01c
murnda <mdelong333@gmail.com>
2019-11-12 01:57:14 -0500

    function to update product inventory functioning

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -77,6 +77,7 @@ function purchasePrompt() {
 
                 if (currentStock >= response.purchaseQuantity) {
                     var purchaseID = response.purchaseID;
+                    
                     var updateStock = currentStock - response.purchaseQuantity;
                     // console.log(updateStock);
 
@@ -126,6 +127,6 @@ function anotherPurchase() {
         } else {
             console.log("Thank you, please come again!");
             connection.end();
-        }
-    })
-}
+        };
+    });
+};
--- bamazonManager.js
+++ bamazonManager.js
@@ -38,22 +38,22 @@ function menuOptions() {
                 break;
             
             case "Add to Inventory":
-                increaseStock();
+                updateStock();
                 break;
 
             case "Add New Product":
                 addNew();
                 break;
 
-            case "exit":
+            case "Exit":
                 connection.end();
-                break;
         };
     });
 };
 
 // If a manager selects View Products for Sale, the app should list every available item: the item IDs, names, prices, and quantities.
 function viewProducts() {
+
     connection.query("SELECT * FROM products", function(err, res) {
         if (err) throw err;
         for (var i = 0; i < res.length; i++) {
@@ -61,14 +61,76 @@ function viewProducts() {
             ${res[i].item_id} | ${res[i].product_name} | ${res[i].department_name} | ${res[i].price} | ${res[i].stock_quantity}
             ----------------------------------------------------`);
         };
+
+        menuOptions();
     });
 };
 
-
 // If a manager selects View Low Inventory, then it should list all items with an inventory count lower than five.
+function viewLow() {
 
+    connection.query("SELECT * FROM products WHERE stock_quantity < 5", function(err, res) {
+        if (err) throw err;
+        for (var i = 0; i < res.length; i++) {
+            console.log(`
+            ${res[i].item_id} | ${res[i].product_name} | ${res[i].stock_quantity}
+            ---------------------------------------`);
+        };
+
+        menuOptions();
+    });
+};
 
 // If a manager selects Add to Inventory, your app should display a prompt that will let the manager "add more" of any item currently in the store.
+function updateStock() {
+    
+    inquirer.prompt([
+        {
+            name: "itemID",
+            message: "Enter the ID of the product you'd like to update",
+            type: "input",
+            filter: Number
+        },
+        {
+            name: "newQuantity",
+            message: "Enter new inventory quantity",
+            type: "input",
+            filter: Number
+        }
+    ])
+    .then(function(res) {
+        var query = "UPDATE products SET ? WHERE ?";
+    
+        connection.query(query, [{stock_quantity: res.newQuantity}, {item_id: res.itemID}], function(err) {
+            if (err) throw err;
+        });
+        
+        console.log(`
+        Product number ${res.itemID} updated to ${res.newQuantity}
+        ---------------------------------------\n`);
 
+        updateMoreStock();
+    });
+};
+
+function updateMoreStock() {
+    inquirer.prompt([
+        {
+            type: "confirm",
+            message: "Would you like to update inventory quantity for another item?",
+            name: "confirm",
+            default: true
+        }
+    ]).then(function(res) {
+        if (res.confirm) {
+            updateStock();
+        } else {
+            menuOptions();
+        };
+    });
+};
 
 // If a manager selects Add New Product, it should allow the manager to add a completely new product to the store.
+function addNew() {
+    
+}
